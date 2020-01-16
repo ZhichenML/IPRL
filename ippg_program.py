48cee433e7170b0e0c195efefbb76147ea4e8354
Zhichen Gong <zhichengong@ZhichendeMacBook-Pro.local>
2020-01-16 17:13:08 +0000

    test pid_parameter

--- ippg_program.py
+++ ippg_program.py
@@ -126,9 +126,9 @@ def learn_policy(track_name):
         # Learn new programmatic policy
         param_finder = ParameterFinder(all_observations, all_actions, steer_prog, accel_prog, brake_prog)
 
-        steer_ranges = [tuple([create_interval(steer_prog.pid_info()[0][const], 0.05) for const in range(3)]), create_interval(steer_prog.pid_info()[1], 0.01)]
-        accel_ranges = [tuple([create_interval(accel_prog.pid_info()[0][const], 0.05) for const in range(3)]), create_interval(accel_prog.pid_info()[1], 0.5), create_interval(accel_prog.pid_info()[2], 0.1), create_interval(accel_prog.pid_info()[3], 0.01)]
-        brake_ranges = [tuple([create_interval(brake_prog.pid_info()[0][const], 0.05) for const in range(3)]), create_interval(brake_prog.pid_info()[1], 0.001)]
+        steer_ranges = [[create_interval(steer_prog.pid_info()[0][const], 0.05) for const in range(3)], create_interval(steer_prog.pid_info()[1], 0.01)]
+        accel_ranges = [[create_interval(accel_prog.pid_info()[0][const], 0.05) for const in range(3)], create_interval(accel_prog.pid_info()[1], 0.5), create_interval(accel_prog.pid_info()[2], 0.1), create_interval(accel_prog.pid_info()[3], 0.01)]
+        brake_ranges = [[create_interval(brake_prog.pid_info()[0][const], 0.05) for const in range(3)], create_interval(brake_prog.pid_info()[1], 0.001)]
         pid_ranges = [steer_ranges, accel_ranges, brake_ranges]
         new_paras = param_finder.pid_parameters(pid_ranges)
 
@@ -143,8 +143,6 @@ def learn_policy(track_name):
     logging.info("Brake Controller" + str(brake_prog.pid_info()))
 
     return None
-
-
 
 
 if __name__ == "__main__":
