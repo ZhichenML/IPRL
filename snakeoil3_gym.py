b9a54255e0c45e6cf5832187a544c7d7111edc5f
Zhichen Gong <zhichengong@ZhichendeMacBook-Pro.local>
2020-01-18 02:44:07 +0000

    fix server error

--- snakeoil3_gym.py
+++ snakeoil3_gym.py
@@ -248,7 +248,6 @@ class Client():
                 sockdata = sockdata.decode('utf-8')
             except socket.error as emsg:
                 print('.e', end=' ')
-                continue
                 return # ???????????
                 #print "Waiting for data on %d.............." % self.port
             if '***identified***' in sockdata:
