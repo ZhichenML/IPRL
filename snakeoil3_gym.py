e1a24c6c895131781c73eb0c342eee5b259b8455
Zhichen Gong <zhichengong@ZhichendeMacBook-Pro.local>
2020-02-09 23:13:05 +0000

    text mode

--- snakeoil3_gym.py
+++ snakeoil3_gym.py
@@ -180,7 +180,7 @@ class Client():
                         #config_string = 'torcs -s -r /usr/local/share/games/torcs/config/raceman/' + self.track_name + ' -T -nofuel &'
                         #os.system(config_string)
                         #os.system('torcs -T -nofuel &')
-                        os.system('torcs -s -r /usr/local/share/games/torcs/config/raceman/' + self.track_name +' -T -nofuel -nodamage -nolaptime &')
+                        os.system('torcs -s -r /usr/local/share/games/torcs/config/raceman/' + self.track_name + ' -T -nofuel -nodamage -nolaptime &')
                         os.system('sh autostart.sh')
                         # os.system('torcs -s -r /usr/local/share/games/torcs/config/raceman/practiceregcg.xml -nofuel -nodamage -nolaptime &')
                     else:
